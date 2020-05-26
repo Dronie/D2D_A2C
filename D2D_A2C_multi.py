eadcca5878ef5ec8beef84a353ac8df1818a66c6
Dronie <sroes002@gold.ac.uk>
2020-05-26 16:41:42 +0100

    added comments

--- D2D_A2C_multi.py
+++ D2D_A2C_multi.py
@@ -11,48 +11,48 @@ ch = D2D.Channel()
 
 # set up Actor and Critic networks
 class ActorCriticNetwork:
-  def __init__(self, name, obs_size=2, action_size=2, actor_hidden_size=32, critic_hidden_size=32, ac_learning_rate=0.001,  
-               entropy_cost=0.01, normalise_entropy=True, lambda_=0., baseline_cost=1.):
+    def __init__(self, name, obs_size=2, action_size=2, actor_hidden_size=32, critic_hidden_size=32, ac_learning_rate=0.001,  
+                 entropy_cost=0.01, normalise_entropy=True, lambda_=0., baseline_cost=1.):
     
-    with tf.variable_scope(name):
-      # hyperparameter bootstrap_n determines the batch size
-      self.name=name
-      self.input_ = tf.placeholder(tf.float32, [None, obs_size], name='inputs')
-      self.action_ = tf.placeholder(tf.int32, [None, 1], name='action')
-      self.reward_ = tf.placeholder(tf.float32, [None, 1], name='reward')
-      self.discount_ = tf.placeholder(tf.float32, [None, 1], name='discount')
-      self.bootstrap_ = tf.placeholder(tf.float32, [None], name='bootstrap')
+        with tf.variable_scope(name):
+            # network variables
+            self.name=name
+            self.input_ = tf.placeholder(tf.float32, [None, obs_size], name='inputs')
+            self.action_ = tf.placeholder(tf.int32, [None, 1], name='action')
+            self.reward_ = tf.placeholder(tf.float32, [None, 1], name='reward')
+            self.discount_ = tf.placeholder(tf.float32, [None, 1], name='discount')
+            self.bootstrap_ = tf.placeholder(tf.float32, [None], name='bootstrap')
 
-      # set up actor network (approximates optimal policy)
-      self.fc1_actor_ = tf.contrib.layers.fully_connected(self.input_, actor_hidden_size, activation_fn=tf.nn.elu)
-      self.fc2_actor_ = tf.contrib.layers.fully_connected(self.fc1_actor_, actor_hidden_size, activation_fn=tf.nn.elu)
-      self.fc3_actor_ = tf.contrib.layers.fully_connected(self.fc2_actor_, action_size, activation_fn=None)
-      # reshape the policy logits
-      self.policy_logits_ = tf.reshape(self.fc3_actor_, [-1, 1, action_size] )
+            # set up actor network (approximates optimal policy)
+            self.fc1_actor_ = tf.contrib.layers.fully_connected(self.input_, actor_hidden_size, activation_fn=tf.nn.elu)
+            self.fc2_actor_ = tf.contrib.layers.fully_connected(self.fc1_actor_, actor_hidden_size, activation_fn=tf.nn.elu)
+            self.fc3_actor_ = tf.contrib.layers.fully_connected(self.fc2_actor_, action_size, activation_fn=None)
+            # reshape the policy logits
+            self.policy_logits_ = tf.reshape(self.fc3_actor_, [-1, 1, action_size] )
   
-      # generate action probabilities for taking actions
-      self.action_prob_ = tf.nn.softmax(self.fc3_actor_)
+            # generate action probabilities for taking actions
+            self.action_prob_ = tf.nn.softmax(self.fc3_actor_)
       
-      # set up critic network (approximates optimal value function (used as a baseline to reduce variance of loss gradient))
-      # - uses policy evaluation (e.g. Monte-Carlo / TD learning) to estimate the advantage
-      self.fc1_critic_ = tf.contrib.layers.fully_connected(self.input_, critic_hidden_size, activation_fn=tf.nn.elu)
-      self.fc2_critic_ = tf.contrib.layers.fully_connected(self.fc1_critic_, critic_hidden_size, activation_fn=tf.nn.elu)
-      self.baseline_ = tf.contrib.layers.fully_connected(self.fc2_critic_, 1, activation_fn=None)
+            # set up critic network (approximates optimal state-value function (used as a baseline to reduce variance of loss gradient))
+            # - uses policy evaluation (e.g. Monte-Carlo / TD learning) to estimate the advantage
+            self.fc1_critic_ = tf.contrib.layers.fully_connected(self.input_, critic_hidden_size, activation_fn=tf.nn.elu)
+            self.fc2_critic_ = tf.contrib.layers.fully_connected(self.fc1_critic_, critic_hidden_size, activation_fn=tf.nn.elu)
+            self.baseline_ = tf.contrib.layers.fully_connected(self.fc2_critic_, 1, activation_fn=None)
       
-      # Calculates the loss for an A2C update along a batch of trajectories. (TRFL)
-      self.seq_aac_return_ = trfl.sequence_advantage_actor_critic_loss(self.policy_logits_, self.baseline_, self.action_,
+            # Calculates the loss for an A2C update along a batch of trajectories. (TRFL)
+            self.seq_aac_return_ = trfl.sequence_advantage_actor_critic_loss(self.policy_logits_, self.baseline_, self.action_,
                self.reward_, self.discount_, self.bootstrap_, lambda_=lambda_, entropy_cost=entropy_cost, 
                baseline_cost=baseline_cost, normalise_entropy=normalise_entropy)
       
-      # Optimize the loss
-      self.ac_loss_ = tf.reduce_mean(self.seq_aac_return_.loss)
-      self.ac_optim_ = tf.train.AdamOptimizer(learning_rate=ac_learning_rate).minimize(self.ac_loss_)
+            # Optimize the loss
+            self.ac_loss_ = tf.reduce_mean(self.seq_aac_return_.loss)
+            self.ac_optim_ = tf.train.AdamOptimizer(learning_rate=ac_learning_rate).minimize(self.ac_loss_)
       
-  def get_network_variables(self):
-      return [t for t in tf.trainable_variables() if t.name.startswith(self.name)]
+    def get_network_variables(self):
+        return [t for t in tf.trainable_variables() if t.name.startswith(self.name)]
 
 # hyperparameters
-train_episodes = 5000  
+max_timesteps = 5000  
 discount = 0.99
 
 actor_hidden_size = 32 # number of units per layer in actor net
@@ -72,7 +72,7 @@ normalise_entropy = True
 # lambda_: an optional scalar or 2-D Tensor with shape `[T, B]` for
 #        Generalised Advantage Estimation as per
 #        https://arxiv.org/abs/1506.02438.
-lambda_ = 1.
+lambda_ = 0.
 
 action_size = ch.n_actions
 obs_size = ch.N_CU
@@ -114,19 +114,8 @@ power_levels = []
 RB_selections = []
 
 g_iB, g_j, G_ij, g_jB, G_j_j = ch.reset()
-#for i in range(0, ch.N_D2D):
-#    action = np.random.randint(0, 299, 1)
-#    power_levels.append(ch.action_space[action][0][0])
-#    RB_selections.append(ch.action_space[action][0][1])
     
-#print(power_levels)
-#print(RB_selections)
-
 state = np.zeros(ch.N_CU)
-
-#CU_SINR = ch.CU_SINR_no_collision(g_iB, power_levels, g_jB, RB_selections)
-
-#print(CU_SINR)
 
 def running_mean(x, N):
     cumsum = np.cumsum(np.insert(x, 0, 0)) 
@@ -135,7 +124,7 @@ def running_mean(x, N):
 with tf.Session() as sess:
     # Initialize variables
     sess.run(tf.global_variables_initializer())
-    total_reward, ep_length, done = 0, 0, 0
+    total_reward, t_length, done = 0, 0, 0
     stats_actor_loss, stats_critic_loss = 0., 0.
     total_loss_list, action_list, action_prob_list, bootstrap_list = [], [], [], []
     rewards_list = []
@@ -149,14 +138,7 @@ with tf.Session() as sess:
     avg_throughput = []
     time_avg_throughput = []
 
-    for ep in range(1, train_episodes): # not actually episodic, just a left over var name from the original code
-        #g_iB, g_j, G_ij, g_jB, G_j_j = ch.reset()
-        
-        #CU_SINR = ch.CU_SINR_no_collision(g_iB, power_levels, g_jB, RB_selections)
-        
-        #state = ch.state(CU_SINR)
-
-        
+    for t in range(1, max_timesteps):
 
         ch.collision_indicator = 0
                      
@@ -186,17 +168,26 @@ with tf.Session() as sess:
         
         collisions.append(ch.collision_indicator)
         
-        D2D_collision_probs.append(collision_var / ep)
+        D2D_collision_probs.append(collision_var / t)
 
         next_state = np.clip(next_state,-1.,1.)
         total_reward += net
 
-        ep_length += 1
+        t_length += 1
 
         # bootstrapping: stopping to update many times in a trajectory
         #  - done by estimating the value of the current state using the critic net
+        #
+        # sutton - "updating the value estimate for a state
+        #           from the estimated values of subsequent states" - given by critic net
+        #
+        #           "Only through bootstrapping do we introduce bias and 
+        #            an asymptotic dependance on the quality of the
+        #            function approximation" - this bias "often reduces variance
+        #                                      and accelerates learning"
 
-        if ep == train_episodes:
+        # bootstrapping procedures (need to fix - bootstrapping happens once every timestep)
+        if t == max_timesteps:
           bootstrap_value = np.zeros((1,),dtype=np.float32)
         else:
           #get bootstrap values
@@ -205,18 +196,16 @@ with tf.Session() as sess:
             bootstrap_values.append(sess.run(D2D_target_nets[i].baseline_, feed_dict={
                 D2D_target_nets[i].input_: np.expand_dims(next_state, axis=0)}))
         
-        #train network
-        
+        # update network parameters
         total_losses = []
         seq_aac_returns = []
         for i in range(0, ch.N_D2D):
-            #print(reward[i])
             _, total_loss, seq_aac_return = sess.run([D2D_nets[i].ac_optim_, D2D_nets[i].ac_loss_, D2D_nets[i].seq_aac_return_], feed_dict={
                 D2D_nets[i].input_: np.expand_dims(state, axis=0),
                 D2D_nets[i].action_: np.reshape(actions[i], (-1, 1)),
                 D2D_nets[i].reward_: np.reshape(reward[i], (-1, 1)),
                 D2D_nets[i].discount_: np.reshape(discount, (-1, 1)),
-                D2D_nets[i].bootstrap_: np.reshape(bootstrap_values[i], (1,)) #np.expand_dims(bootstrap_value, axis=0)
+                D2D_nets[i].bootstrap_: np.reshape(bootstrap_values[i], (1,))
             })
             total_losses.append(total_loss)
             seq_aac_returns.append(seq_aac_return)
@@ -224,19 +213,16 @@ with tf.Session() as sess:
         total_loss_list.append(np.mean(total_losses))
 
         
-        #update target network
+        # update target network
         for i in range(0, ch.N_D2D):
             sess.run(D2D_target_net_update_ops[i])
         
-        #some useful things for debuggin
+        # debugging variables
         stats_actor_loss += np.mean(seq_aac_return.extra.policy_gradient_loss)
         stats_critic_loss += np.mean(seq_aac_return.extra.baseline_loss)
         action_list.append(actions)
         bootstrap_list.append(bootstrap_values)
         action_prob_list.append(action_probs)
-        
-        #if total_reward < -250:
-        #  done = 1
 
         a = list(ch.accessed_CUs)
         if 2 in a:
@@ -255,36 +241,28 @@ with tf.Session() as sess:
                 throughput.append(0.0)
         
         access_ratios.append(len(accessed) / len(reward))
-        access_rates.append(sum(access_ratios) / ep)
+        access_rates.append(sum(access_ratios) / t)
 
         avg_throughput.append(sum(throughput))
-        time_avg_throughput.append(sum(avg_throughput)/ ep)
+        time_avg_throughput.append(sum(avg_throughput)/ t)
         
-        if ep % stats_every == 0 or ep == 1:
-            #for i in range(0, ch.N_D2D):
-            #print('Last State: ', state)
+        if t % stats_every == 0 or t == 1:
             print('Power Levels: ', power_levels)
             print('RB Selections: ', RB_selections)
             print('Accessed CUs: ', ch.accessed_CUs)
             print('Reward of colliding agent: ', reward[b])
             print('Number of Collisions: ', ch.collision_indicator)
-            print('||(Ep)isode: {}|| '.format(ep),
-                  #'(T)otal reward: {:.5f}| '.format(np.mean(stats_rewards_list[-stats_every:],axis=0)[1]),
+            print('||(T)imestep): {}|| '.format(t),
                   'Last net (r)eward: {:.3f}| '.format(net),
-                  #'Ep length: {:.1f}| '.format(np.mean(stats_rewards_list[-stats_every:],axis=0)[2]),
                   '(L)oss: {:4f}|'.format(np.mean(total_loss_list)))
-                  #'Actor loss: {:.5f}'.format(stats_actor_loss),
-                  #'Critic loss: {:.5f}'.format(stats_critic_loss))
-            #print(np.mean(bootstrap_value), np.mean(action_list), np.mean(action_prob_list,axis=0))
-        #stats_actor_loss, stats_critic_loss = 0, 0
-        #total_loss_list = []
-        stats_rewards_list.append((ep, total_reward, ep_length))
+
+        stats_rewards_list.append((t, total_reward, t_length))
         rewards_list.append(net)
         state = next_state
 
         #writer.add_graph(sess.graph)
         #print("Graph added!")
-    eps, rews, lens = np.array(stats_rewards_list).T
+    ts, rews, lens = np.array(stats_rewards_list).T
     smoothed_rews = running_mean(rewards_list, 100)
     smoothed_col_probs = running_mean(D2D_collision_probs, 100)
     smoothed_access_rates = running_mean(access_rates, 100)
@@ -292,38 +270,41 @@ with tf.Session() as sess:
 
     reward_fig = plt.figure()
 
-    plt.plot(eps[-len(smoothed_rews):], smoothed_rews)
-    plt.plot(eps, rewards_list, color='grey', alpha=0.3)
+    plt.plot(ts[-len(smoothed_rews):], smoothed_rews)
+    plt.plot(ts, rewards_list, color='grey', alpha=0.3)
     plt.xlabel('Time-slot')
     plt.ylabel('Reward')
     plt.show()
 
     collision_prob_fig = plt.figure()
     plt.ylim(0, 1)
-    plt.plot(eps[-len(smoothed_col_probs):], smoothed_col_probs)
-    plt.plot(eps, D2D_collision_probs, color='grey', alpha=0.3)
+    plt.xlim(0, 5000)
+    plt.plot(ts[-len(smoothed_col_probs):], smoothed_col_probs)
+    plt.plot(ts, D2D_collision_probs, color='grey', alpha=0.3)
     plt.xlabel('Time-slot')
     plt.ylabel('D2D collision probability')
     plt.show()
 
     true_collisions_fig = plt.figure()
     plt.ylim(0, 1)
-    plt.plot(eps, collisions)
+    plt.xlim(0, 5000)
+    plt.plot(ts, collisions)
     plt.ylabel('Number of collisions')
     plt.xlabel('Time-slot')
     plt.show()
 
     access_rate_fig = plt.figure()
     plt.ylim(0, 1)
-    plt.plot(eps[-len(smoothed_access_rates):], smoothed_access_rates)
-    plt.plot(eps, access_rates, color='grey', alpha=0.3)
+    plt.xlim(0, 5000)
+    plt.plot(ts[-len(smoothed_access_rates):], smoothed_access_rates)
+    plt.plot(ts, access_rates, color='grey', alpha=0.3)
     plt.xlabel('Time-slot')
     plt.ylabel('D2D access rate')
     plt.show()
 
     time_avg_overall_thrghpt_fig = plt.figure()
-    plt.plot(eps[-len(smoothed_throughput):], smoothed_throughput)
-    plt.plot(eps, time_avg_throughput, color='grey', alpha=0.3)
+    plt.plot(ts[-len(smoothed_throughput):], smoothed_throughput)
+    plt.plot(ts, time_avg_throughput, color='grey', alpha=0.3)
     plt.xlabel('Time-slot')
     plt.ylabel('Time-averaged network throughput')
     plt.show()
